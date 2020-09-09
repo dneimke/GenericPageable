108448d1a45f108c7efefbc4656742e0c0ea4053
Darren Neimke <darren.neimke@readify.net>
2020-09-09 22:12:11 +0930

    Moved filter into view component

---
+++ GenericPageable/Views/Shared/Components/Filter/Filter.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GenericPageable.Views.Shared.Components.Filter
+{
+    public class Filter : ViewComponent
+    {
+        public IViewComponentResult Invoke(string action)
+        {
+            return View("Default", action);
+        }
+    }
+}
